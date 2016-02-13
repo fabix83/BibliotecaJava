859398bcecc2b3b8da28a39a5bd22bfa447386ff
FaRo <FaRo@DESKTOP-RB0A552>
2016-02-13 13:31:38 -0300

    Criação da Classe Ebook - Herança

---
+++ src/Ebook.java
@@ -0,0 +1,21 @@
+/*
+ * Exemplo de Herança - Classe Ebooks é uma extensão da Classe Livro
+ */
+
+
+public class Ebook extends Livro {
+	private String waterMark;
+	
+	public Ebook (Autor autor) {
+		super(autor);
+	}
+
+	public String getWaterMark() {
+		return waterMark;
+	}
+
+	public void setWaterMark(String waterMark) {
+		this.waterMark = waterMark;
+	}
+	
+}
